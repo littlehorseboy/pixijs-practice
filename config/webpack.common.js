fd656c2297863186dc8e50e1b1452342380b046c
littlehorseboy <littlehorseboy@gmail.com>
2019-08-10 00:29:35 +0800

    feat: catImg scaleToWindow

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -23,6 +23,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+            },
+          },
+        ],
+      },
     ],
   },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 import 'normalize.css';
 import * as PIXI from 'pixi.js';
+import scaleToWindow from './assets/js/scaleToWindow';
 
+const catImg = require('./assets/images/cat.png'); // eslint-disable-line @typescript-eslint/no-var-requires
 
-let type = 'WebGL';
-
-if (!PIXI.utils.isWebGLSupported()) {
-  type = 'canvas';
-}
-
-PIXI.utils.sayHello(type);
+const {
+  Application,
+  Loader,
+  Sprite,
+  resources,
+} = PIXI;
 
 const app = new PIXI.Application({
-  width: 256,
-  height: 256,
   antialias: true,
+});
+
+window.addEventListener('resize', (): void => {
+  scaleToWindow(app.renderer.view);
 });
 
 document.body.appendChild(app.view);
@@ -23,3 +26,22 @@ app.renderer.view.style.position = 'absolute';
 app.renderer.view.style.display = 'block';
 app.renderer.autoResize = true;
 app.renderer.resize(window.innerWidth, window.innerHeight);
+
+const setup = (): void => {
+  console.log('setup');
+
+  const sprite = PIXI.Sprite.from(catImg);
+
+  app.stage.addChild(sprite);
+};
+
+const loadProgressHandler = (loader, resource): void => {
+  console.log(`loading ${resource.url}`);
+  console.log(`progress ${loader.progress} %`);
+};
+
+const loader = new Loader();
+loader
+  .add(catImg)
+  .on('progress', loadProgressHandler)
+  .load(setup);
