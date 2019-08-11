a2f5aa5a627f4f3d79e3c0c4336ab5b4b43557c6
littlehorseboy <littlehorseboy@gmail.com>
2019-08-11 23:35:13 +0800

    feat: 鍵盤移動

--- src/assets/js/keyboard.ts
+++ src/assets/js/keyboard.ts
@@ -18,25 +18,30 @@ export default function keyboard(keyCode: number): KeyI {
     downHandler(event): void {
       event.preventDefault();
       if (event.keyCode === key.code) {
-        if (key.isDown && key.release) {
-          key.release();
+        if (key.isUp && key.press) {
+          key.press();
         }
-        key.isDown = false;
-        key.isUp = true;
+
+        key.isDown = true;
+        key.isUp = false;
       }
     },
     upHandler(event): void {
       event.preventDefault();
 
       if (event.keyCode === key.code) {
-        if (key.isUp && key.press) {
-          key.press();
+        if (key.isDown && key.release) {
+          key.release();
         }
-        key.isDown = true;
-        key.isUp = false;
+
+        key.isDown = false;
+        key.isUp = true;
       }
     },
   };
+
+  window.addEventListener('keydown', key.downHandler.bind(key), false);
+  window.addEventListener('keyup', key.upHandler.bind(key), false);
 
   return key;
 }
